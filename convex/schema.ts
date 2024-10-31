e11e3d7204c03dd92f05a2ee9dbb2fa03e28a28d
Pranav <chhabrapranav2001@gmail.com>
2024-10-31 14:49:27 -0500

    Linked Clerk with convex

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,6 +2,13 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+  users: defineTable({
+    username: v.string(),
+    image: v.string(),
+    clerkId: v.string(),
+  }).index("by_clerk_id", ["clerkId"])
+    .searchIndex("search_username", { searchField: "username" })
+    .searchIndex("search_image", { searchField: "image" }),
   messages: defineTable({
     sender: v.string(),
     content: v.string(),
