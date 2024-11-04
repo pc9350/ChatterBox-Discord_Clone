6279f16c003f9272dffbc9aded27f61b24048a6c
Pranav <chhabrapranav2001@gmail.com>
2024-11-03 18:45:28 -0600

    Added messages functionality

---
+++ src/app/(dashboard)/_components/new-direct-message.tsx
@@ -0,0 +1,67 @@
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { SidebarGroupAction } from "@/components/ui/sidebar";
+import { PlusIcon } from "lucide-react";
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+export function NewDirectMessage() {
+  const [open, setOpen] = useState(false);
+  const createDirectMessage = useMutation(api.functions.dm.create);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const id = await createDirectMessage({ username: e.currentTarget.username.value });
+      setOpen(false);
+      router.push(`/dms/${id}`);
+    } catch (error) {
+      toast.error("Failed to create Direct Message", {
+        description:
+          error instanceof Error ? error.message : "An Unknown error occured",
+      });
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <SidebarGroupAction>
+          <PlusIcon />
+          <span className="sr-only">New Direct Message</span>
+        </SidebarGroupAction>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>New Direct Message</DialogTitle>
+          <DialogDescription>
+            Enter a username to start a direct message.
+          </DialogDescription>
+        </DialogHeader>
+        <form className="contents" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-1">
+            <Label>Username</Label>
+            <Input id="username" type="text" />
+          </div>
+          <DialogFooter>
+            <Button>Start Direct Message</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/app/(dashboard)/_components/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar.tsx
@@ -2,88 +2,113 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarGroupAction,
-    SidebarGroupContent,
-    SidebarGroupLabel,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { SignOutButton } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 import { PlusIcon, User2Icon } from "lucide-react";
 import Link from "next/link";
 import { api } from "../../../../convex/_generated/api";
+import { NewDirectMessage } from "./new-direct-message";
+import { usePathname } from "next/navigation";
+
+const useTestDirectMessages = () => {
+  const user = useQuery(api.functions.user.get);
+  if (!user) return [];
+  return [user, user, user];
+};
 
 export function DashboardSidebar() {
-    const user = useQuery(api.functions.user.get);
-  
-    if (!user) {
-      return null;
-    }
-  
-    return (
-      <Sidebar>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                <SidebarMenuItem>
-                  <SidebarMenuButton asChild>
-                    <Link href="/friends">
-                      <User2Icon />
-                      Friends
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              </SidebarMenu>
-            </SidebarGroupContent>
-            <SidebarGroup>
-              <SidebarGroupLabel>Direct Messages</SidebarGroupLabel>
-              <SidebarGroupAction>
-                <PlusIcon />
-                <span className="sr-only">New Direct Message</span>
-              </SidebarGroupAction>
-            </SidebarGroup>
-          </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter>
-          <SidebarGroup>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                <SidebarMenuItem>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <SidebarMenuButton>
-                        <Avatar className="size-6">
-                          <AvatarImage src={user?.image} />
-                          <AvatarFallback>{user.username[0]}</AvatarFallback>
-                        </Avatar>
-                        <p className="font-medium">{user.username}</p>
-                      </SidebarMenuButton>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                      <DropdownMenuItem asChild>
-                        <SignOutButton />
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </SidebarMenuItem>
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarFooter>
-      </Sidebar>
-    );
+  const user = useQuery(api.functions.user.get);
+  const directMessages = useQuery(api.functions.dm.list);
+  const pathname = usePathname();
+
+  if (!user) {
+    return null;
   }
-  
+
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild isActive={pathname === "/"}>
+                  <Link href="/friends">
+                    <User2Icon />
+                    Friends
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Direct Messages</SidebarGroupLabel>
+            <NewDirectMessage />
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {directMessages?.map((directMessage) => (
+                  <SidebarMenuItem key={directMessage._id}>
+                    <SidebarMenuButton asChild isActive={pathname === `/dms/${directMessage._id}`}>
+                      <Link href={`/dms/${directMessage._id}`}>
+                        <Avatar className="size-6">
+                          <AvatarImage src={directMessage.user.image} />
+                          <AvatarFallback>
+                            {directMessage.user.username[0]}
+                          </AvatarFallback>
+                        </Avatar>
+                        <p className="font-medium">{directMessage.user.username}</p>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <SidebarMenuButton>
+                      <Avatar className="size-6">
+                        <AvatarImage src={user?.image} />
+                        <AvatarFallback>{user.username[0]}</AvatarFallback>
+                      </Avatar>
+                      <p className="font-medium">{user.username}</p>
+                    </SidebarMenuButton>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem asChild>
+                      <SignOutButton />
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
